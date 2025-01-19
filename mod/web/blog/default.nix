e181e1e1a154b280a2dc045d1d38b13d7f0ef2bc
Kleidi Bujari <mail@4kb.net>
2025-01-19 14:06:11 -0500

    switch blog to symlink with others

--- mod/web/blog/default.nix
+++ mod/web/blog/default.nix
@@ -1,15 +1,30 @@
-{ pkgs, ... }: pkgs.stdenvNoCC.mkDerivation {
-  pname = "4kb.net";
-  version = "1.0";
-  src = ./.;
+{ pkgs, depot, ... }:
+let
+  inherit (pkgs)
+    symlinkJoin
+    ;
 
-  nativeBuildInputs = with pkgs; [
-    zola
+  posts = pkgs.stdenvNoCC.mkDerivation {
+    pname = "4kb.net";
+    version = "1.0";
+    src = ./.;
+
+    nativeBuildInputs = with pkgs; [
+      zola
+    ];
+
+    buildPhase = "zola build";
+    installPhase = ''
+      mkdir -p $out
+      cp -r public/* $out/
+    '';
+  };
+
+in
+symlinkJoin {
+  name = "site";
+  paths = [
+    posts
+    depot.misc.cv
   ];
-
-  buildPhase = "zola build";
-  installPhase = ''
-    mkdir -p $out
-    cp -r public/* $out/
-  '';
 }
