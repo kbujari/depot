c2923f015452a54402df906dc7aa0b21fd016f0b
Kleidi Bujari <mail@4kb.net>
2026-07-15 18:53:32 -0700

    init default.nix for no flake ops

---
+++ default.nix
@@ -0,0 +1,45 @@
+let
+  defaultNixpkgs = builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
+    ref = "nixos-unstable";
+    rev = "9ae611a455b90cf061d8f332b977e387bda8e1ca";
+    shallow = true;
+  };
+in
+{
+  pkgs ? import defaultNixpkgs { },
+  localSystem ? builtins.currentSystem,
+  crossSystem ? null,
+  ...
+}:
+
+let
+  inherit (pkgs) lib;
+
+  inherit (lib)
+    packagesFromDirectoryRecursive
+    ;
+
+  overlay =
+    final: _:
+    let
+      depotPackages = packagesFromDirectoryRecursive {
+        callPackage = lib.callPackageWith (final // { inherit depotPackages; });
+        directory = ./packages;
+      };
+    in
+    {
+      inherit depotPackages;
+    };
+
+  newPkgs = import defaultNixpkgs {
+    config = {
+      allowUnfree = true;
+      allowUnfreeRedistributable = true;
+    };
+
+    overlays = [ overlay ];
+  };
+
+in
+newPkgs.depotPackages
